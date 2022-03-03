3fd86b895c2a420cb8b055d5b6c8b8da5439348d
hazratamin98 <hazratamin98@gmail.com>
2022-03-03 11:14:49 +0500

    split done

--- src/counter/counter.js
+++ src/counter/counter.js
@@ -18,12 +18,14 @@ const Counter = () => {
           placeholder="start typing"
         ></textarea>
       </div>
-      <div>
+      <div className=" result_wrapper ">
         <h2> words : {text.toString().split(" ").length}</h2>
         <h2> Character : {text.length} </h2>
       </div>
-      <h3>Text Preview</h3>
-      <p>{text}</p>
+      <div className="text_wrapper">
+        <h3>Text Preview</h3>
+        <p>{text}</p>
+      </div>
     </div>
   )
 }
