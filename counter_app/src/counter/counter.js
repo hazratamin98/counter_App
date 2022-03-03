24e307cfdc21cbfabd69fc3502beb93ac85d1627
hazratamin98 <hazratamin98@gmail.com>
2022-03-03 09:51:38 +0500

    character count

--- counter_app/src/counter/counter.js
+++ counter_app/src/counter/counter.js
@@ -1,18 +1,19 @@
-import React from "react"
-import { useState } from "react"
+import React from "react";
+import { useState } from "react";
 
 const Counter = () => {
   const handleOnchange = (event) => {
-    setText(event.target.value)
-  }
+    setText(event.target.value);
+  };
 
-  const [text, setText] = useState(0)
+  const [text, setText] = useState(0);
 
   return (
     <div className="counter_wrapper">
       <h1>Counter</h1>
       <div>
         <textarea
+          value={text}
           onChange={handleOnchange}
           placeholder="start typing"
         ></textarea>
@@ -23,8 +24,10 @@ const Counter = () => {
           {text.split(" ").length} words {text.length} Character{" "}
         </h2>
       </div>
+      <h3>Text Preview</h3>
+      <p>{text}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
